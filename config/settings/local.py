3681c327fecd663a3625fdf5b1120cd8d9fe4f71
Emmanuel Ramirez Badillo <emanolorb@gmail.com>
2026-06-18 11:44:57 -0600

    add super user

--- config/settings/local.py
+++ config/settings/local.py
@@ -57,7 +57,8 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
+USE_DOCKER = "no"
+if USE_DOCKER == "yes":
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
